cd196150f8942240e463615507f04e4170c4ec96
Ayush <ayushdalal291206@gmail.com>
2024-12-04 19:35:56 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose'
 export const connectDB = async () => {
     try {
-        const conn = await mongoose.connect("mongodb+srv://ayushdalal291206:wQIXbygoY3TwZYee@cluster0.1ynkl.mongodb.net/products?retryWrites=true&w=majority&appName=Cluster0")
+        const conn = await mongoose.connect(process.env.MONGO_URI)
         console.log(`MongoDB connected: ${conn.connection.host}`)
     } catch (error) {
         console.error(`${error}`)
